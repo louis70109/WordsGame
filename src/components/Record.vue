ba8aa86435f70c0eb2169b23c2c2e536f58a74ab
louis70109 <nijia.lin@linecorp.com>
2021-11-21 05:32:51 +0800

    add record page

---
+++ src/components/Record.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="record">
+  </span>
+</template>
+
+<script>
+import { userCheck, lineLogout } from '../utils/users';
+export default {
+  setup() {
+    let url = process.env.VUE_APP_API_URL;
+    function login() {
+      fetch(url + '/login/uri').then((res) => {
+        res.json().then((el) => {
+          window.location.replace(el.result);
+        });
+      });
+    }
+    return { login, userCheck, lineLogout };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Barrage from '../components/Barrage.vue';
 import LineLogin from '../components/LineLogin.vue';
 import LineAuth from '../components/LineAuth.vue';
+import Record from '../components/Record.vue';
 const routerHistory = createWebHistory();
 
 const router = createRouter({
@@ -18,6 +19,10 @@ const router = createRouter({
     {
       path: '/line/auth',
       component: LineAuth,
+    },
+    {
+      path: '/record',
+      component: Record,
     },
   ],
 });
