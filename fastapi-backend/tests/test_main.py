4ada2c584b90c4becd015a5d77b3ab2f172b779f
louis70109 <nijia.lin@linecorp.com>
2021-11-24 21:18:36 +0800

    backup

--- fastapi-backend/tests/test_main.py
+++ fastapi-backend/tests/test_main.py
@@ -2,9 +2,7 @@ import unittest
 from unittest import mock
 from testcontainers.postgres import PostgresContainer
 import os
-with PostgresContainer("postgres:9.5").with_bind_ports(5432, 47000) as postgres:
-    os.environ['DATABASE_URI'] = postgres.get_connection_url()
-    DB_URI = postgres.get_connection_url()
+
 import sqlalchemy
 from _pytest.monkeypatch import MonkeyPatch
 from mock import patch
@@ -15,29 +13,30 @@ from main import app
 
 from sql_app.database import Base, get_db
 
+with PostgresContainer("postgres:9.5").with_bind_ports(5432, 47000) as postgres:
+    # os.environ['DATABASE_URI'] = postgres.get_connection_url()
+    DB_URI = postgres.get_connection_url()
+    engine = create_engine(DB_URI)
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    # postgres.get_container_host_ip()
+    Base.metadata.create_all(bind=engine)
+
+
+    def override_get_db():
+        try:
+            db = TestingSessionLocal()
+            yield db
+        finally:
+            db.close()
+
+
+    app.dependency_overrides[get_db] = override_get_db()
 
 print("@@@@@@@@@@@@@@@@")
 print(DB_URI)
 print()
 print("@@@@@@@@@@@@@@@@")
-engine = create_engine(DB_URI)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db()
 client = TestClient(app)
 
 
@@ -47,46 +46,14 @@ def test_health_check():
     assert response.json() == {"message": "Hello World!"}
 
 
-# def test_get_db_check():
-#     import sqlalchemy
-#
-# with PostgresContainer("postgres:9.5") as postgres:
-#         DB_URI = postgres.get_connection_url()
-#         os.environ["DATABASE_URI"] = f'postgres://{postgres.POSTGRES_USER}:{postgres.POSTGRES_PASSWORD}@{postgres.get_container_host_ip()}:65235/{postgres.POSTGRES_DB}'
-
-
-    # DB_URI = f'postgres://{postgres.POSTGRES_USER}:{postgres.POSTGRES_PASSWORD}@{postgres.get_container_host_ip()}:65235/{postgres.POSTGRES_DB}'
-
-
-#         # postgresql: // postgres: postgres @ db / postgres
-#         # postgresql + psycopg2: // test: test @ localhost:65186 / test
-#         print(postgres.get_connection_url())
-#         print(os.getenv("DATABASE_URI"))
-#         response = client.get("/users")
-#         print(response.json())
-
-
 class TestClient(unittest.TestCase):
     def setUp(self):
         pass
-        # with PostgresContainer("postgres:9.5") as postgres:
-        #     print(os.getenv("DATABASE_URI"))
-        #     self.env = EnvironmentVarGuard()
-        #     self.env.set('DATABASE_URI', postgres.get_connection_url())
-        # with PostgresContainer("postgres:9.5") as postgres:
-        #     print(os.getenv("DATABASE_URI"))
-        #
-        #     os.environ["DATABASE_URI"] = postgres.get_connection_url()
-
-        # postgresql: // postgres: postgres @ db / postgres
-        # postgresql + psycopg2: // test: test @ localhost:65186 / test
-        # print(postgres.get_connection_url())
 
     def test_read_main(self):
         print("___________________-")
         print(os.getenv('DATABASE_URI'))
-        import time
-        time.sleep(10000)
+        # need to create table first
         response = client.get("/users")
 
         print(response.json())
