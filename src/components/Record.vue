a22ddd7cba36a06d11bbd0d722600ba12dc69da3
louis70109 <nijia.lin@linecorp.com>
2021-11-28 20:56:06 +0800

    fix onmounted property render problem with reactive function

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,37 +1,37 @@
 <template>
-  <span class="record">
+  <div class="record">
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">{{ records }}</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
-  </span>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">{{ records.value }}</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import {onMounted, ref} from 'vue'
+import { reactive, onMounted } from 'vue';
 
 export default {
   setup() {
     const url = process.env.VUE_APP_API_URL;
-    let records = ref(null);
-    onMounted(async ()=>{
-      records = await (await fetch(url+'/games/')).json()
-      console.log(records);
-    })
+    let records = reactive([]);
+    onMounted(async () => {
+      const games = await (await fetch(url + '/games/')).json();
+      records.value = games
+    });
     return { records };
   },
 };
